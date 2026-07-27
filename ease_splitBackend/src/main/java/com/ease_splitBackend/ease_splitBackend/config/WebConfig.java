c5b53814274455fd7f06cdbc43ffabb704c2f8b5
kd-7117-2127 <krish.deep7117@gmail.com>
2026-07-27 16:22:22 +0530

    ready for cloud deployment

--- ease_splitBackend/src/main/java/com/ease_splitBackend/ease_splitBackend/config/WebConfig.java
+++ ease_splitBackend/src/main/java/com/ease_splitBackend/ease_splitBackend/config/WebConfig.java
@@ -9,16 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods(
-                        "GET",
-                        "POST",
-                        "PUT",
-                        "DELETE",
-                        "OPTIONS"
-                )
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
